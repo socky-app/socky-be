c379eb555a3e0c1e4b01b61471d6d1df4cf781a9
Lucas T. R. Schneider <lucastrschneider@gmail.com>
2026-01-11 14:24:40 -0300

    Add login route to app and update quick dev

--- examples/quick_dev.rs
+++ examples/quick_dev.rs
@@ -1,10 +1,20 @@
 use anyhow::Result;
+use serde_json::json;
 
 #[tokio::main]
 async fn main() -> Result<()> {
     let client = httpc_test::new_client("http://localhost:8080")?;
 
     client.do_get("/hello").await?.print().await?;
+
+    let req_login = client.do_post(
+        "/api/login",
+        json!({
+            "username": "test1",
+            "password": "welcome"
+        }),
+    );
+    req_login.await?.print().await?;
 
     Ok(())
 }
--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,12 @@
 
 use std::net::SocketAddr;
 
-use axum::{response::Html, routing::get, Router};
+use axum::{
+    middleware,
+    response::{Html, Response},
+    routing::get,
+    Router,
+};
 use tokio::net::TcpListener;
 use tracing::info;
 use tracing_subscriber::EnvFilter;
@@ -11,6 +16,7 @@ pub use self::error::{Error, Result};
 
 mod config;
 mod error;
+mod web;
 
 /// Entrypoint for the backend service
 #[tokio::main]
@@ -21,10 +27,10 @@ async fn main() -> Result<()> {
         .with_env_filter(EnvFilter::from_default_env())
         .init();
 
-    let app = Router::new().route(
-        "/hello",
-        get(|| async { Html("Hello <strong>World!!!</strong>") }),
-    );
+    let app = Router::new()
+        .merge(routes_hello())
+        .merge(web::routes_login::routes())
+        .layer(middleware::map_response(main_response_mapper));
 
     // Start server
     let addr = SocketAddr::from(([127, 0, 0, 1], 8080));
@@ -33,4 +39,20 @@ async fn main() -> Result<()> {
     axum::serve(listener, app).await.unwrap();
 
     Ok(())
+}
+
+async fn main_response_mapper(res: Response) -> Response {
+    println!("->> {:<12} - main_response_mapper", "RES_MAPPER");
+    println!();
+    res
+}
+
+fn routes_hello() -> Router {
+    Router::new().route(
+        "/hello",
+        get(|| async {
+            println!("->> {:<12} - hello", "HANDLER");
+            Html("Hello <strong>World!!!</strong>")
+        }),
+    )
 }
