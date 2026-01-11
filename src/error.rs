5b7b7fb485ccf70dae81fbbafb8c6d3a1bb1a477
Lucas T. R. Schneider <lucastrschneider@gmail.com>
2026-01-11 20:07:56 -0300

    Add client-side error and update main response mapper

--- src/error.rs
+++ src/error.rs
@@ -9,7 +9,7 @@ use axum::{
 pub type Result<T> = core::result::Result<T, Error>;
 
 /// Main crate error
-#[derive(Debug, Clone)]
+#[derive(Debug, Clone, strum_macros::AsRefStr)]
 pub enum Error {
     // Config
     ConfigMissingEnv(&'static str),
@@ -24,7 +24,17 @@ pub enum Error {
     LoginFail,
 
     // Model
-    TransactionDeleteFailIdNotFound{ id: u64 },
+    TransactionDeleteFailIdNotFound { id: u64 },
+}
+
+/// Client side error
+#[derive(Debug, strum_macros::AsRefStr)]
+#[allow(non_camel_case_types)]
+pub enum ClientError {
+    LOGIN_FAIL,
+    NO_AUTH,
+    INVALID_PARAMS,
+    SERVICE_ERROR,
 }
 
 impl fmt::Display for Error {
@@ -42,6 +52,37 @@ impl IntoResponse for Error {
     fn into_response(self) -> Response {
         println!("->> {:<12} - {self:?}", "INTO_RES");
 
-        (StatusCode::INTERNAL_SERVER_ERROR, "UNHANDLED_CLIENT_ERROR").into_response()
+        // Create a placeholder Axum response
+        let mut response = StatusCode::INTERNAL_SERVER_ERROR.into_response();
+
+        // Insert the Error into the response
+        response.extensions_mut().insert(self);
+
+        response
+    }
+}
+
+impl Error {
+    pub fn client_status_and_error(&self) -> (StatusCode, ClientError) {
+        #[allow(unreachable_patterns)]
+        match self {
+            // Login
+            Self::LoginFail => (StatusCode::FORBIDDEN, ClientError::LOGIN_FAIL),
+
+            // Auth
+            Self::AuthFailNoAuthTokenCookie
+            | Self::AuthFailWrongTokenFormat
+            | Self::AuthFailCtxNotInRequestExt => (StatusCode::FORBIDDEN, ClientError::NO_AUTH),
+
+            // Model
+            Self::TransactionDeleteFailIdNotFound { .. } => {
+                (StatusCode::BAD_REQUEST, ClientError::INVALID_PARAMS)
+            }
+            // Fallback
+            _ => (
+                StatusCode::INTERNAL_SERVER_ERROR,
+                ClientError::SERVICE_ERROR,
+            ),
+        }
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,18 @@
 //! Backend of the Socky application
 
+#![allow(unused)] // for development only.
+
 use std::net::SocketAddr;
 
 use axum::{
-    middleware,
-    response::{Html, Response},
-    routing::get,
-    Router,
+    Json, Router, middleware, response::{Html, IntoResponse, Response}, routing::get
 };
+use serde_json::json;
 use tokio::net::TcpListener;
 use tower_cookies::CookieManagerLayer;
 use tracing::info;
 use tracing_subscriber::EnvFilter;
+use uuid::Uuid;
 
 use crate::model::ModelController;
 
@@ -58,8 +59,33 @@ async fn main() -> Result<()> {
 
 async fn main_response_mapper(res: Response) -> Response {
     println!("->> {:<12} - main_response_mapper", "RES_MAPPER");
+
+    let uuid = Uuid::new_v4();
+
+    // Get eventual response error
+    let service_error = res.extensions().get::<Error>();
+    let client_status_error = service_error.map(|se| se.client_status_and_error());
+
+    // Build new response
+    let error_response = client_status_error
+        .as_ref()
+        .map(|(status_code, client_error)| {
+            let client_error_body = json!({
+                "error": {
+                    "type": client_error.as_ref(),
+                    "req_uuid": uuid.to_string(),
+                }
+            });
+
+            println!("  ->> client_error_body: {client_error_body}");
+            (*status_code, Json(client_error_body)).into_response()
+        });
+
+    // TODO: build and log server log line
+    println!("  ->> server log line - {uuid} - Error: {service_error:?}");
+
     println!();
-    res
+    error_response.unwrap_or(res)
 }
 
 fn routes_hello() -> Router {
