a2f01a15350d668118a1a0ba8f49b131e6846749
Lucas T. R. Schneider <lucastrschneider@gmail.com>
2026-01-18 19:40:35 -0300

    Create new middleware for response mapper

--- src/main.rs
+++ src/main.rs
@@ -2,21 +2,19 @@
 
 use std::net::SocketAddr;
 
-use axum::{
-    http::{Method, Uri},
-    middleware,
-    response::{Html, IntoResponse, Response},
-    routing::get,
-    Json, Router,
-};
-use serde_json::json;
+use axum::{middleware, response::Html, routing::get, Router};
 use tokio::net::TcpListener;
 use tower_cookies::CookieManagerLayer;
 use tracing::{debug, info};
 use tracing_subscriber::EnvFilter;
-use uuid::Uuid;
 
-use crate::{ctx::Ctx, log::log_request, model::ModelController};
+use crate::{
+    model::ModelController,
+    web::{
+        mw_auth::{mw_ctx_resolver, mw_require_auth},
+        mw_res_map::mw_res_map,
+    },
+};
 
 pub use self::error::{Error, Result};
 
@@ -40,18 +38,15 @@ async fn main() -> Result<()> {
     let mc = ModelController::new().await?;
 
     let routes_api = web::routes_transaction::routes(mc.clone())
-        .route_layer(middleware::from_fn(web::mw_auth::mw_require_auth));
+        .route_layer(middleware::from_fn(mw_require_auth));
 
     // Create app Router
     let app = Router::new()
         .merge(routes_hello())
         .merge(web::routes_login::routes())
         .nest("/api", routes_api)
-        .layer(middleware::map_response(main_response_mapper))
-        .layer(middleware::from_fn_with_state(
-            mc.clone(),
-            web::mw_auth::mw_ctx_resolver,
-        ))
+        .layer(middleware::map_response(mw_res_map))
+        .layer(middleware::from_fn_with_state(mc.clone(), mw_ctx_resolver))
         .layer(CookieManagerLayer::new());
 
     // Start server
@@ -61,42 +56,6 @@ async fn main() -> Result<()> {
     axum::serve(listener, app).await.unwrap();
 
     Ok(())
-}
-
-async fn main_response_mapper(
-    ctx: Result<Ctx>,
-    uri: Uri,
-    req_method: Method,
-    res: Response,
-) -> Response {
-    debug!("{:<12} - main_response_mapper", "RES_MAPPER");
-
-    let uuid = Uuid::new_v4();
-
-    // Get eventual response error
-    let service_error = res.extensions().get::<Error>();
-    let client_status_error = service_error.map(|se| se.client_status_and_error());
-
-    // Build new response
-    let error_response = client_status_error
-        .as_ref()
-        .map(|(status_code, client_error)| {
-            let client_error_body = json!({
-                "error": {
-                    "type": client_error.as_ref(),
-                    "req_uuid": uuid.to_string(),
-                }
-            });
-
-            debug!("{:<12}\n{client_error_body}", "CLIENT ERROR BODY");
-            (*status_code, Json(client_error_body)).into_response()
-        });
-
-    let client_error = client_status_error.unzip().1;
-    let _ = log_request(uuid, req_method, uri, ctx.ok(), service_error, client_error).await;
-
-    debug!("\n");
-    error_response.unwrap_or(res)
 }
 
 fn routes_hello() -> Router {
--- src/web/mod.rs
+++ src/web/mod.rs
@@ -1,6 +1,7 @@
 //! Web utilities and route registrations for the application.
 
 pub mod mw_auth;
+pub mod mw_res_map;
 pub mod routes_login;
 pub mod routes_transaction;
 
