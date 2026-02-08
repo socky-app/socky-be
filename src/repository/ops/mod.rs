56fbccdcef3e8b00f9e0e690cdc355d991acdb12
Lucas T. R. Schneider <lucastrschneider@gmail.com>
2026-02-08 14:47:08 -0300

    Add soft delete and generic exists by column function

--- src/repository/ops/mod.rs
+++ src/repository/ops/mod.rs
@@ -8,7 +8,31 @@ pub mod create;
 pub mod get;
 pub mod update;
 pub mod delete;
+pub mod soft_delete;
 
 pub trait DatabaseTable {
     const TABLE: &'static str;
+
+    /// Generic check for existence based on a column and value
+    async fn exists_by_column(
+        rm: RepositoryManager, 
+        column: &str, 
+        value: &str
+    ) -> Result<bool> {
+        let sql = format!(
+            "SELECT EXISTS(SELECT 1 FROM {} WHERE {} = $1 AND deleted_at IS NULL)",
+            Self::TABLE,
+            column
+        );
+        
+        let exists = sqlx::query_scalar::<_, bool>(&sql)
+            .bind(value)
+            .fetch_one(rm.pool())
+            .await
+            .inspect_err(|e| {
+                tracing::error!("Database error checking existance on {}: {:?}", Self::TABLE, e);
+            })?;
+
+        Ok(exists)
+    }
 }
---
+++ src/repository/ops/soft_delete.rs
@@ -0,0 +1,38 @@
+use crate::repository::ops::DatabaseTable;
+use crate::repository::{Error, RepositoryManager, Result};
+use chrono::Utc;
+use sqlx::postgres::Postgres;
+use sqlx::{Executor, QueryBuilder};
+
+use std::fmt::Debug;
+
+pub trait SoftDelete: DatabaseTable + Sized {
+    async fn soft_delete(rm: &RepositoryManager, id: i64) -> Result<()> {
+        soft_delete::<Self, _>(id, rm.pool()).await
+    }
+}
+
+pub async fn soft_delete<'c, R, E>(id: i64, executor: E) -> Result<()>
+where
+    R: DatabaseTable,
+    E: Executor<'c, Database = sqlx::Postgres>,
+{
+    let query = sqlx::query_scalar::<_, i64>(
+        "UPDATE users SET deleted_at = $1 WHERE id = $2 AND deleted_at IS NULL RETURNING id",
+    )
+    .bind(Utc::now().naive_utc())
+    .bind(id);
+
+    let deleted = query.fetch_all(executor).await.inspect_err(|e| {
+        tracing::error!("Database error soft deleting {}: {:?}", R::TABLE, e);
+    })?;
+
+    if deleted.is_empty() {
+        Err(Error::NotFound {
+            entity: R::TABLE,
+            id,
+        })
+    } else {
+        Ok(())
+    }
+}
