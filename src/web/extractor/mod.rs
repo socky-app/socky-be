b93d2b891b502b546bd087f3a239901f6c05bd23
Lucas T. R. Schneider <lucastrschneider@gmail.com>
2026-02-19 22:28:02 -0300

    Add documentation to extractor mod

--- src/web/extractor/mod.rs
+++ src/web/extractor/mod.rs
@@ -1,3 +1,9 @@
+//! Extractor types to handle role access.
+//! 
+//! Use axum::middleware::from_extractor::<T>() to create middlewares when multiple routes
+//! have the same permissions, or use the extractor directly on the route if more
+//! granular access is required.
+
 use crate::model::user::UserRole;
 
 mod require_role;
