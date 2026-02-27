5269f3f9d59300309c546a4296a0403d3c9325c5
Lucas T. R. Schneider <lucastrschneider@gmail.com>
2026-02-26 23:27:32 -0300

    Fix config error display implementation

--- src/config/error.rs
+++ src/config/error.rs
@@ -2,8 +2,8 @@ use std::fmt;
 
 use thiserror::Error;
 
-// TODO: Fix Display
 #[derive(Debug, Error)]
+#[error("invalid configuration: {}", details.join("; "))]
 pub struct ConfigError {
     pub details: Vec<String>,
 }
@@ -16,29 +16,5 @@ impl From<figment::Error> for ConfigError {
                 .collect::<Vec<_>>();
 
             ConfigError { details }
-    }
-}
-
-impl fmt::Display for ConfigError {
-    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-        writeln!(f, "\n====================================================")?;
-        writeln!(f, "❌ CONFIGURATION ERROR")?;
-        writeln!(f, "====================================================")?;
-
-        for (i, msg) in self.details.iter().enumerate() {
-            writeln!(f, "{}. Issue: {}", i + 1, msg)?;
-        }
-
-        writeln!(f, "----------------------------------------------------")?;
-        writeln!(f, "💡 TROUBLESHOOTING:")?;
-        writeln!(f, "Ensure these Environment Variables are set (double __ for nesting):")?;
-        writeln!(f, "   - SOCKY_DB__USER")?;
-        writeln!(f, "   - SOCKY_DB__PASSWORD")?;
-        writeln!(f, "   - SOCKY_DB__HOST")?;
-        writeln!(f, "   - SOCKY_AUTH__PASSWORD_PEPPER")?;
-        writeln!(f, "   - SOCKY_AUTH__ACCESS_TOKEN_SECRET")?;
-        writeln!(f, "   - SOCKY_AUTH__REFRESH_TOKEN_SECRET")?;
-        writeln!(f, "   - SOCKY_AUTH__REFRESH_TOKEN_PEPPER")?;
-        writeln!(f, "====================================================")
     }
 }
